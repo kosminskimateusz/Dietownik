1162f97020e5a46338ac8f7a6cd77e4afe104a29
kosminskimateusz <kosminskimateusz@gmail.com>
2021-01-12 14:49:58 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dietownik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
