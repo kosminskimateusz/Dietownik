18caf28bf24ae51a69262a6f275417cbdd1ec428
kosminskimateusz <kosminskimateusz@gmail.com>
2021-01-13 12:33:10 +0100

    Added to class ProductManager.cs methods: Start(){ AddProduct(), AddProductToDataBase() }, AllProducts()

--- Class/ProductManager.cs
+++ Class/ProductManager.cs
@@ -1,10 +1,88 @@
+using System;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Collections.Generic;
+
+
 namespace Dietownik
 {
     class ProductManager
     {
-        public void AddProduct(Product product)
+        private string Folder = "./Products/";
+        private string Extention = ".json";
+        Product NewProduct { get; set; }
+        List<Product> AllProductsList = new List<Product>();
+        public void Start()
         {
-            // Dodaj produkt do folderu Products/ nazwa pliku  jak nazwaProduktu.json
+            string name = "";
+            Console.WriteLine("Type name of product:\t");
+            // name = Console.ReadLine();
+            name = "Marchewka";
+            AddProduct(name);
+            AddProductToDataBase();
+        }
+        private void AddProduct(string name)
+        {
+            // Create product object
+            NewProduct = new Product(name);
+            // Call method product.AddProductToDataBase(product);
+        }
+        private void AddProductToDataBase()
+        {
+            bool fileAdded = false;
+            string fileName = "";
+            string path = "";
+            // string jsonString;
+
+            do
+            {
+                fileName = NewProduct.Name;
+                path = Folder + fileName + Extention;
+                // Create productName.json file, Add all informations (Name, Kcal, Fat etc...) in json format. 
+                if (!File.Exists(path))
+                {
+                    var options = new JsonSerializerOptions
+                    {
+                        WriteIndented = true,
+                    };
+                    byte[] jsonUtf8Bytes;
+                    jsonUtf8Bytes = JsonSerializer.SerializeToUtf8Bytes(NewProduct, options);
+                    // jsonString = JsonSerializer.Serialize(NewProduct);
+                    File.WriteAllBytes(path, jsonUtf8Bytes);
+                    // File.WriteAllText(path, jsonString);
+                    Console.WriteLine("Dodano nowy produkt do bazy danych.");
+                    fileAdded = true;
+                }
+                else
+                {
+                    Console.WriteLine("Produkt o podanej nazwie juz istnieje.");
+                    Console.WriteLine("Podaj inna nazwe");
+                    NewProduct.Name = Console.ReadLine();
+                }
+            } while (fileAdded == false);
+        }
+
+        public List<Product> AllProducts()
+        {
+            // var json = File.ReadAllText(Folder + "*.json");
+            // var productX = JsonSerializer.Deserialize<Product>(json);
+            foreach (string file in Directory.GetFiles(Folder, "*.json"))
+            {
+
+                // Product product;
+                using (StreamReader sr = new StreamReader(file))
+                {
+                    var jsonBytes = File.ReadAllBytes(file);
+                    var personX = JsonSerializer.Deserialize<Product>(jsonBytes);
+                    System.Console.WriteLine(personX.Name);
+                    if (personX != null)
+                    {
+                        AllProductsList.Add(personX);
+                    }
+                }
+            }
+            return AllProductsList;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Dietownik
 {
@@ -6,7 +7,17 @@ namespace Dietownik
     {
         static void Main(string[] args)
         {
-            
+            ProductManager productManager = new ProductManager();
+            productManager.Start();
+            var listProducts = productManager.AllProducts();
+
+            // var products = listProducts.Select(product => product.Name).ToList();
+            // var products = listProducts.Where(product => product != null).Select(product => product).ToList();
+
+            // foreach (var product in products)
+            // {
+            //     System.Console.WriteLine(product.Name);
+            // }
         }
     }
 }
